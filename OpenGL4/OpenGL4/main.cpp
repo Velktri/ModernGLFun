06267b7253af458621076d3f37fb3f2d9737a46f
Velktri <murrayg2@gmail.com>
2017-03-30 19:03:57 -0700

    cleaned up main

--- OpenGL4/OpenGL4/main.cpp
+++ OpenGL4/OpenGL4/main.cpp
@@ -1,21 +1,12 @@
-#include "World.h"
-#include "Input.h"
-#include "UI\Layout.h"
-#include "FrameBuffer.h"
 #include "Engine.h"
-#include <imgui.h>
-#include "imgui_impl_sdl_gl3.h"
 
 int main(int argc, char *argv[]) {
-
 	Engine* engine = new Engine();
 	if (!engine->Init()) {
 		return -1;
 	}
 
 	engine->Run();
-
 	engine->CleanUp();
-
 	return 0;
 }
